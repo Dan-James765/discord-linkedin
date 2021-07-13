baef5f3cd2243a4e557f5200b43660507511cc40
Dan James <danjameshh@hotmail.com>
2021-07-13 12:12:43 +0100

    Commit

--- src/app/components/Trending.js
+++ src/app/components/Trending.js
@@ -4,7 +4,11 @@ function Trending() {
     return (
         <>
         <Header/> 
-    
+        <div className="flex items-center justify-center pt-10">
+        <div className="border  w-96  py-4 rounded-lg flex flex-col h-64 shadow-xl">
+
+        </div>
+        </div>
 
         
             
